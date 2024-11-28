97328db8beb210219a94357477d26200798c97ef
AzureAD\【Egrowth】SanpeiMasah <sanpei@egrowth-inc.com>
2024-11-28 23:49:05 +0900

    すべての変更をコミット

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/リポジトリ名/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
